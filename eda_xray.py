d0de6682d6d94166820f40b0ebc470abb0732d5f
ymans88 <yasharmansouri99@gmail.com>
2019-11-17 13:37:43 -0500

    initial commit files are added, pre EDA done, images are ready for adaptive histogram equalization

---
+++ eda_xray.py
@@ -0,0 +1,12 @@
+import pandas as pd
+def load_xray(data):
+    df = pd.read_csv(data)
+    # dropping last empty column
+    df.columns = ['imgindex', 'label', 'followup', 'patientID',
+                  'age', 'gender', 'viewposition', 'width',
+                  'height]', 'x', 'y', 'drop']
+    df = df.drop(df.columns[-1], axis=1)
+
+    df['path'] = 'data/images/'
+    df['path'] = df['path'] + df['imgindex']
+    return df
